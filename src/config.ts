c89da1f88a10745effd21aef45e7c05edea010fa
Eka Prasetia <ekaone3033@gmail.com>
2024-02-02 12:10:29 +0700

    posts commit

---
+++ src/config.ts
@@ -0,0 +1,147 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://posts.prasetia.me/", // replace this with your deployed domain
+  author: "Eka Prasetia",
+  desc: "Dedicated to crafting products with focus on intricate details.",
+  title: "Eka Prasetia",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/ekaone",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://facebook.com",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://instagram.com",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:ekaone3033@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/twekaone",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: false,
+  },
+  {
+    name: "Twitch",
+    href: "https://twitch.tv",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://youtube.com",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://whatsapp.com",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://snapchat.com",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://Pinterest.com",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://TikTok.com",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://CodePen.com",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://Discord.com",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://GitLab.com",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://Reddit.com",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://Skype.com",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://Steam.com",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://telegram.com",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://mastodon.com",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,30 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+As a passionate web development enthusiast based in [Jakarta](https://www.google.com/maps/place/Jakarta), I constantly seek out new challenges and embrace the ever-evolving tech landscape. Whether it's experimenting with the latest web technologies, delving into innovative coding techniques, or contributing to exciting projects, I thrive on the dynamic nature of the digital realm.
+
+Living at the intersection of creativity and technology, I'm on a perpetual quest to learn, grow, and bring fresh perspectives to the vibrant web development community.
+
+<div>
+  <img src="/assets/dev.svg" class="sm:w-1/2 mx-auto" alt="coding dev illustration">
+</div>
+
+## Tech Stack
+
+In my web development journey, I leverage a versatile tech stack that enables me to bring ideas to life seamlessly. Primarily, I harness the power of JavaScript, employing its dynamic capabilities for frontend and backend development alike.
+
+## Features
+
+Here are certain Technology.
+
+- [ReactJS](https://react.dev/)
+- NodeJS
+- Bun
+- Hono
+- OpenAI
+- Vertex AI
+- Anthropic
+- API
+- Supabase
